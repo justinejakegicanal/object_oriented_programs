487dcd98e39d59354d3c8556b2dd45f96f62f6c9
Jake <justinejake16@gmail.com>
2026-04-28 00:44:03 +0800

    I initialized the setup for the number separator of class and variables

---
+++ even_odd_separator/even_odd_separator.py
@@ -0,0 +1,5 @@
+class NumberSeparator:
+    def __init__(self, source_file, even_file, odd_file):
+        self.source_file = source_file
+        self.even_file = even_file
+        self.odd_file = odd_file
