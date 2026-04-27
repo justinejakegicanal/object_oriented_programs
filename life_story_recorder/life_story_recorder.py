5fd6d7f4c3ec2c311cd462d5f961d1693774bca2
Jake <justinejake16@gmail.com>
2026-04-28 05:12:30 +0800

    setting up the file writing block to handle saving the story entries

--- life_story_recorder/life_story_recorder.py
+++ life_story_recorder/life_story_recorder.py
@@ -1,3 +1,7 @@
 class LifeStoryRecorder:
     def __init__(self, log_file="mylife.txt"):
         self.log_file = log_file
+
+    def record_entries(self):
+        with open(self.log_file, 'w') as file:
+            pass 
