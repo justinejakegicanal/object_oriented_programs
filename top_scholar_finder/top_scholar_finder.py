f2eb1c2fcf62db91b28ff562d4066d9e6c633152
Jake <justinejake16@gmail.com>
2026-04-28 02:16:46 +0800

    I established file reading loop and tracking variables for the highest scholar para maangas

--- top_scholar_finder/top_scholar_finder.py
+++ top_scholar_finder/top_scholar_finder.py
@@ -1,3 +1,11 @@
 class TopScholarFinder:
     def __init__(self, student_records_file):
         self.student_records_file = student_records_file
+
+    def get_highest_scholar(self):
+        top_scholar_name = "None"
+        best_grade = 5.0  
+
+        with open(self.student_records_file, 'r') as file:
+            for line in file:
+                pass 
