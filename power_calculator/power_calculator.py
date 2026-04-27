b536f64680301eb6907eba047762f6cd14cf3c0f
Jake <justinejake16@gmail.com>
2026-04-28 06:40:44 +0800

    I initialized the calculator class and prepared the script to handle input/output files

---
+++ power_calculator/power_calculator.py
@@ -0,0 +1,5 @@
+class PowerCalculator:
+    def __init__(self, source_numbers, squares_file, cubes_file):
+        self.source_numbers = source_numbers
+        self.squares_file = squares_file
+        self.cubes_file = cubes_file
