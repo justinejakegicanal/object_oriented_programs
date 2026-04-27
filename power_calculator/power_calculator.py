ed9f8a75214da49b12fabd26768e9d23c8e5df78
Jake <justinejake16@gmail.com>
2026-04-28 06:45:54 +0800

    calculate squares for even numbers and cubes for odd numbers

--- power_calculator/power_calculator.py
+++ power_calculator/power_calculator.py
@@ -7,3 +7,6 @@ class PowerCalculator:
     def apply_powers(self):
         with open(self.source_numbers, 'r') as file:
             all_numbers = [int(line.strip()) for line in file if line.strip()]
+
+        squared_evens = [str(num**2) for num in all_numbers if num % 2 == 0]
+        cubed_odds = [str(num**3) for num in all_numbers if num % 2 != 0]
